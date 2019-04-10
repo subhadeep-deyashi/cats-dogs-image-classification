0865fa453963f187b9e567fb2fff36cc43ede982
Subhadeep Deyashi <subhadeepdeyashi1@gmail.com>
2019-04-11 02:23:12 +0530

    Add files via upload

---
+++ catsdogs.py
@@ -0,0 +1,152 @@
+import os
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+local_weights_file = '/Users/subhadeep.deyashi/Desktop/program/ml/cats_and_dogs/tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+pre_trained_model = InceptionV3(
+    input_shape=(150, 150, 3), include_top=False, weights=None)
+pre_trained_model.load_weights(local_weights_file)
+
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
+last_layer = pre_trained_model.get_layer('mixed7')
+print ('last layer output shape:'+ str(last_layer.output_shape))
+last_output = last_layer.output
+
+
+from tensorflow.keras.optimizers import RMSprop
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(last_output)
+# Add a fully connected layer with 1,024 hidden units and ReLU activation
+x = layers.Dense(1024, activation='relu')(x)
+# Add a dropout rate of 0.2
+x = layers.Dropout(0.2)(x)
+# Add a final sigmoid layer for classification
+x = layers.Dense(1, activation='sigmoid')(x)
+
+# Configure and compile the model
+model = Model(pre_trained_model.input, x)
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.0001),
+              metrics=['acc'])
+
+
+
+import os
+import zipfile
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+local_zip = '/Users/subhadeep.deyashi/Desktop/program/ml/cats_and_dogs/tmp/cats_and_dogs_filtered.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/Users/subhadeep.deyashi/Desktop/program/ml/cats_and_dogs/tmp')
+zip_ref.close()
+
+# Define our example directories and files
+base_dir = '/Users/subhadeep.deyashi/Desktop/program/ml/cats_and_dogs/tmp/cats_and_dogs_filtered'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=50,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.3,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir, # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+
+from tensorflow.keras.optimizers import SGD
+
+unfreeze = False
+
+# Unfreeze all models after "mixed6"
+for layer in pre_trained_model.layers:
+  if unfreeze:
+    layer.trainable = True
+  if layer.name == 'mixed6':
+    unfreeze = True
+
+# As an optimizer, here we will use SGD 
+# with a very low learning rate (0.00001)
+model.compile(loss='binary_crossentropy',
+              optimizer=SGD(
+                  lr=0.00001, 
+                  momentum=0.9),
+              metrics=['acc'])
+
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=50,
+      validation_data=validation_generator,
+      validation_steps=50,
+      verbose=2)
+
+
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Retrieve a list of accuracy results on training and test data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+# Plot training and validation loss per epoch
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
