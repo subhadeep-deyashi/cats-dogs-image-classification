3fd2789c13471e3b3dce54ce618398fbf5ee18ca
Subhadeep Deyashi <subhadeepdeyashi1@gmail.com>
2020-05-11 19:15:09 +0530

    Update catsdogs.py

--- catsdogs.py
+++ catsdogs.py
@@ -5,6 +5,7 @@ from tensorflow.keras import Model
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 
 local_weights_file = '/tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
 pre_trained_model = InceptionV3(
     input_shape=(150, 150, 3), include_top=False, weights=None)
 pre_trained_model.load_weights(local_weights_file)
